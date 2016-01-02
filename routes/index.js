a9315c99454ebfa00b4001ff0995e9b73aa0e425
patm1987 <pux1987@gmail.com>
2016-01-02 11:56:35 -0700

    I now grab a random spoiler from a mysql database

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var SpoilerProvider = require('../spoiler_provider');
 var router = express.Router();
-var spoilerProvider = new SpoilerProvider();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Spoiler Roulette', spoiler: spoilerProvider.getSpoiler() });
+	var spoilerProvider = new SpoilerProvider();
+	spoilerProvider.getSpoiler(function(spoilerTitle, spoilerBody) {
+		res.render('index', {
+			title: 'Spoiler Roulette',
+			spoilerTitle: spoilerTitle,
+			spoiler: spoilerBody
+		});
+	});
 });
 
 module.exports = router;
--- spoiler_provider.js
+++ spoiler_provider.js
@@ -2,12 +2,30 @@
  * Created by Patrick on 1/2/2016.
  */
 
+var mysql = require('mysql');
+
 function SpoilerProvider() {
-	this._spoiler = 'Jar Jar is Greedo\'s spy';
 }
 
-SpoilerProvider.prototype.getSpoiler = function() {
-	return this._spoiler;
+SpoilerProvider.prototype.getSpoiler = function(spoilerCB) {
+	var sqlConnection = mysql.createConnection({
+		host: 'localhost',
+		user: 'spoiler_select',
+		password: '',
+		database: 'spoilers'
+	});
+	sqlConnection.connect();
+	sqlConnection.query(
+		'SELECT * FROM spoilers ORDER BY RAND() LIMIT 1',
+		function(err, rows, fields) {
+			if (err) {
+				spoilerCB('Error!', 'Error: ' + err);
+			}
+			else {
+				spoilerCB(rows[0].title, rows[0].body);
+			}
+		});
+	sqlConnection.end();
 };
 
 module.exports = SpoilerProvider;
