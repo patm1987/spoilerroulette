b6ac6e2afc6c820ba7c68461af2aac71f2499f5a
patm1987 <pux1987@gmail.com>
2016-01-02 12:24:09 -0700

    New Spoilers!

--- model/SELECT_t___FROM_spoilers_spoilers_t.sql
+++ model/SELECT_t___FROM_spoilers_spoilers_t.sql
@@ -1,2 +1,11 @@
-INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Jar Jar''s Past', 'Jar Jar was the original Knight of Ren.', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Jar Jar''s Past', 'Jar Jar was one of the original Knights of Ren, establishing not just a pair but a high council of Siths.', 0);
 INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('It''s a Trap', 'Admiral Ackbar dies after escaping the First Order''s armada. He did not realize that he was, in fact, falling into a trap!', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Ren''s Parents', 'Kylo Ren is the offspring of Han and Leiah, led to the darkside despite the best efforts of Luke.', 1);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Rey''s Parents', 'Rey is the grandchild of Supreme Leader Snoke, her family hid her on Jakku trying to flee Snoke''s bounty hunters.', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Finn''s Parents', 'Finn is Boba Fett''s son, he was sold to the First Order after his father''s disappearance.', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Han Solong', 'So long Han! He dies trying to convert his son from the dark side.', 1);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Chewey''s Doom', 'Chewbacca dies trying to land the Millenium Falcon during a daring attack on Star Base Killer. After heroically landing the falcon, a harpoon is thrust through the cockpit into the copilot''s seat.', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('C-3P0''s Arm', 'C-3P0 had his arm modified to serve as Leiah''s guardian. Its displayed capabilities include improved strength, speed, and some sort of concussive projectile.', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('BB-D2', 'Although R2 appears to be disabled midway through the movie, he has actually been transferred into BB-8''s body to aid in the recovery of Luke''s location.', 0);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Poe''s Untimely Death', 'Despite appearing to die in a Tie Fighter crash, Poe actually survives to save the day two more times. AKA: the soap opera death!', 1);
+INSERT INTO spoilers.spoilers (title, body, factual) VALUES ('Stay for the Credits', 'If you stay after the credits, a mysterious figure is seen rescuing Han Solo from his apparent death.', 0);
