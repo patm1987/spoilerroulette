6326aae2e4382cd7f0b5b2fe5926877469074d9b
patm1987 <pux1987@gmail.com>
2016-01-02 10:33:28 -0700

    I made sure you can pass a spoiler into jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Spoiler Roulette' });
+  res.render('index', { title: 'Spoiler Roulette', spoiler: 'Grand Moff is Luke\'s uncle!' });
 });
 
 module.exports = router;
